7940acc7e33e38b1a82efd345e8e949d38c087e8
Manuel Finelli <jfinelli@redhat.com>
2021-08-20 09:59:50 +0100

    Removed the Jira-checking routine

--- index.js
+++ index.js
@@ -3,16 +3,12 @@ var fromArray = require("from2-array");
 var jenkinsBuildsStream = require("./streams/jenkinsBuildsStream");
 var jenkinsBuildsInfoStream = require("./streams/jenkinsBuildsInfoStream");
 var jenkinsFailedBuildsFilterStream = require("./streams/jenkinsFailedBuildsFilterStream.js");
-var jiraIssuesStream = require("./streams/jiraIssuesStream.js");
-var jiraInfoStream = require("./streams/jiraInfoStream.js")(config.jiraHost);
 var failuresAggregatorStream = require("./streams/failuresAggregatorStream.js");
 
 fromArray.obj(config.jobs)
     .pipe(jenkinsBuildsStream)
     .pipe(jenkinsBuildsInfoStream)
     .pipe(jenkinsFailedBuildsFilterStream)
-    .pipe(jiraIssuesStream)
-    .pipe(jiraInfoStream)
     .pipe(failuresAggregatorStream);
 
 var nunjucksReportGenerator = require("./generators/nunjucksReportGenerator");
